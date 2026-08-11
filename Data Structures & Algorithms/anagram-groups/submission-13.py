0b65c7e61fbd17535db71640cd46e61357e1c518
Ryan Russell <48161363+rhrussell@users.noreply.github.com>
2026-08-11 12:42:22 -0500

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        word_map = defaultdict(list)
+
+        for word in strs:
+            char_map = [0] * 26
+            for char in word:
+                char_map[ord(char) - ord('a')] += 1
+            word_map[tuple(char_map)].append(word) # you can use tuples as keys but cant use dictionaries or maps
+
+        return list(word_map.values())
+ 
